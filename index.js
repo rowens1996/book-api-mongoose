83871b6912445e31ea2b8c4731b172cfb66fc74a
Ryan <96872747+rowens1996@users.noreply.github.com>
2022-02-21 09:52:01 +0000

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./router");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const mongoose = require("mongoose");
 const uri =
